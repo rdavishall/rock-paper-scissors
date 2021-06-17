6f5cb156604bd1150fcdd9dbc80de85a8d8a994d
Rushton Davis-Hall <rdavishall@gmail.com>
2021-06-17 10:59:57 -0600

    Added a reset button, animations to images, dynamic images added based on computer choice

--- Dom-version/app.js
+++ Dom-version/app.js
@@ -1,7 +1,12 @@
-let computerSelection;
-let humanSelection;
-let computerWins = 0;
-let humanWins = 0;
+let humanScore = 0;
+let cpuScore = 0;
+const buttons = document.querySelectorAll('#human-weapons .btn');
+const rotateImgs = document.querySelectorAll('#human-weapons img');
+const restart = document.querySelector('.restart');
+let cpuWeapons = document.getElementById("cpu-weapons");
+let cpuButton = document.createElement("button");
+let img = document.createElement("img");
+console.log(restart);
 
 // Random Computer Selection
 let computerPlay = function () {
@@ -10,44 +15,93 @@ let computerPlay = function () {
   return selectionArray[randInt];
 }
 
+// Disable button 
+function disableBtn() {
+  buttons.forEach(button => button.disabled = true)
+  rotateImgs.forEach(rotateImg => rotateImg.classList.add('noanimation'))
+}
+
+// Game Rules / Determine Winner
+function playRound() {
+  let computerSelection = computerPlay();
+  // Using computer selection to create element and import image
+  if (computerSelection === "rock") {
+    img.src = "./img/rock.png";
+    cpuWeapons.appendChild(cpuButton);
+    cpuButton.appendChild(img);
+  }
+  else if (computerSelection === "paper") {
+    img.src = "./img/paper.png";
+    cpuWeapons.appendChild(cpuButton);
+    cpuButton.appendChild(img);
+  }
+  else if (computerSelection === "scissors") {
+    img.src = "./img/scissors.png";
+    cpuWeapons.appendChild(cpuButton);
+    cpuButton.appendChild(img);
+  }
+  let gameText = "";
+
+  // Determine Winner
+  if ((humanSelection === 'rock' && computerSelection === 'scissors') ||
+    (humanSelection === 'paper' && computerSelection === 'rock') ||
+    (humanSelection === 'scissors' && computerSelection === 'paper')) {
+    humanScore++;
+    // Set max games to 5 --> will need some type of reset
+    if (humanScore === 5) {
+      gameText = `Congrats you win ${humanScore} - ${cpuScore}`
+      disableBtn();
+    } else {
+      gameText = `You win ${humanSelection} beats ${computerSelection}`;
+    }
+  }
+  else if (humanSelection === computerSelection) {
+    gameText = `Tie game`;
+  }
+  else {
+    cpuScore++;
+    if (cpuScore === 5) {
+      gameText = `Sorry you lose ${cpuScore} - ${humanScore}`
+      disableBtn();
+    } else {
+      gameText = `You lose ${computerSelection} beats ${humanSelection}`;
+    }
+  }
+
+  // Add score and text into HTML document
+  document.getElementById('human-score').innerHTML = humanScore;
+  document.getElementById('cpu-score').innerHTML = cpuScore;
+  document.getElementById('text-content').innerHTML = gameText;
+}
 // Select Human Choice with Click
-const buttons = document.querySelectorAll('.left-side .btn');
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
     humanSelection = button.id;
-    console.log(humanSelection);
-    computerSelection = computerPlay();
-    console.log(computerSelection);
-    playRound(humanSelection, computerSelection);
+    playRound();
   })
 })
 
-// Play rock, paper scissors and for each win add one to total wins
-let playRound = function (humanSelection, computerSelection) {
-  if (humanSelection === "rock" && computerSelection === "scissors") {
-    humanWins = humanWins + 1;
-    console.log("Human Wins");
-    return humanWins;
-  } else if (humanSelection === "paper" && computerSelection === "rock") {
-    humanWins = humanWins + 1;
-    console.log("Human Wins");
-    return humanWins;
-  } else if (humanSelection === "scissors" && computerSelection === "paper") {
-    humanWins = humanWins + 1;
-    console.log("Human Wins");
-    return humanWins;
-  } else if (humanSelection === "scissors" && computerSelection === "scissors") {
-    console.log(`tie game`);
-  } else if (humanSelection === "rock" && computerSelection === "rock") {
-    console.log(`tie game`);
-  } else if (humanSelection === "paper" && computerSelection === "paper") {
-    console.log(`tie game`);
-  } else if (humanSelection !== "rock" && humanSelection !== "scissors" && humanSelection !== "paper") {
-    console.log("Invalid entry: please enter rock, paper or scissors");
-  } else {
-    computerWins = computerWins + 1;
-    console.log("Computer Wins");
-    return computerWins;
-  }
-}
+// Rotate on click 
+rotateImgs.forEach((rotateImg) => {
+  rotateImg.addEventListener('click', () => {
+    rotateImg.classList.add('spin');
+    setTimeout(() => {
+      rotateImg.classList.remove("spin")
+    }, 500)
+  })
+})
+
+// Restart Game
+restart.addEventListener('click', () => {
+  document.getElementById('human-score').innerHTML = 0;
+  document.getElementById('cpu-score').innerHTML = 0;
+  document.getElementById('text-content').innerHTML = "Rock Beats Scissors | Scissors Beats Paper | Paper Beats Rock"
+  rotateImgs.forEach(rotateImg => rotateImg.classList.remove('noanimation'));
+  img.remove();
+  cpuButton.remove();
+  humanScore = 0;
+  cpuScore = 0;
+  buttons.forEach(button => button.disabled = false);
+})
+
 
---
+++ README.md
@@ -0,0 +1,20 @@
+# Project Title
+
+The Odin Project: Rock, Paper, Scissors
+
+## Description
+
+Created a rock paper scissors game with an interactive user interface that plays
+
+## Learnings
+* Selecting elements from the DOM
+* Looping through node lists
+* Adding click events
+* Adding animations on click events
+* Adding and removing classes and IDs
+* Adding elements (div, buttons, images)
+
+## Author
+
+Rushton Davis-Hall
+
