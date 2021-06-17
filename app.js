9a11f79b71e8c3ec866e1ad2d7549bd796d27896
Rushton Davis-Hall <rdavishall@gmail.com>
2021-06-17 11:23:08 -0600

    test

--- app.js
+++ app.js
@@ -1,70 +1,107 @@
-// Starting Conditions
-let humanWins = 0;
-let computerWins = 0;
+let humanScore = 0;
+let cpuScore = 0;
+const buttons = document.querySelectorAll('#human-weapons .btn');
+const rotateImgs = document.querySelectorAll('#human-weapons img');
+const restart = document.querySelector('.restart');
+let cpuWeapons = document.getElementById("cpu-weapons");
+let cpuButton = document.createElement("button");
+let img = document.createElement("img");
+console.log(restart);
 
-// Randomlly Generate Computer Selection
+// Random Computer Selection
 let computerPlay = function () {
-  // Possible Selections
   let selectionArray = ["rock", "paper", "scissors"];
-  // Randomlly select number between 0 & 2
   let randInt = Math.floor(Math.random() * selectionArray.length);
-  // Use selected number to pick the index of the array
   return selectionArray[randInt];
 }
 
-// Play rock, paper scissors and for each win add one to total wins
-let playRound = function (humanSelection, computerSelection) {
-  if (humanSelection === "rock" && computerSelection === "scissors") {
-    humanWins = humanWins + 1;
-    console.log("Human Wins");
-    return humanWins;
-  } else if (humanSelection === "paper" && computerSelection === "rock") {
-    humanWins = humanWins + 1;
-    console.log("Human Wins");
-    return humanWins;
-  } else if (humanSelection === "scissors" && computerSelection === "paper") {
-    humanWins = humanWins + 1;
-    console.log("Human Wins");
-    return humanWins;
-  } else if (humanSelection === "scissors" && computerSelection === "scissors") {
-    console.log(`tie game`)
-  } else if (humanSelection === "rock" && computerSelection === "rock") {
-    console.log(`tie game`)
-  } else if (humanSelection === "paper" && computerSelection === "paper") {
-    console.log(`tie game`)
-  } else if (humanSelection !== "rock" && humanSelection !== "scissors" && humanSelection !== "paper") {
-    console.log("Invalid entry: please enter rock, paper or scissors");
-  } else {
-    computerWins = computerWins + 1;
-    console.log("Computer Wins");
-    return computerWins;
-  }
+// Disable button 
+function disableBtn() {
+  buttons.forEach(button => button.disabled = true)
+  rotateImgs.forEach(rotateImg => rotateImg.classList.add('noanimation'))
 }
 
-// Round Test
-// console.log(playRound("rock", computerPlay()));
-
-// Keep playing while computer wins and human wins < 5
-let game = function () {
-  while (humanWins < 5 && computerWins < 5) {
-    computerSelection = computerPlay();
-    humanSelection = prompt("Enter rock, paper or scissors")
-    humanSelection = humanSelection.toLowerCase();
-    playRound(humanSelection, computerSelection);
-    console.log(`Human wins: ${humanWins}`);
-    console.log(`Computer wins: ${computerWins}`);
+// Game Rules / Determine Winner
+function playRound() {
+  let computerSelection = computerPlay();
+  // Using computer selection to create element and import image
+  if (computerSelection === "rock") {
+    img.src = "./img/rock.png";
+    cpuWeapons.appendChild(cpuButton);
+    cpuButton.appendChild(img);
   }
-};
-
-let winnerMessage = function () {
-  if (humanWins === 5) {
-    console.log(`Human Wins The Game ${humanWins}:${computerWins}`)
-  } else if (computerWins === 5) {
-    console.log(`Computer Wins The Game ${computerWins}:${humanWins}`)
+  else if (computerSelection === "paper") {
+    img.src = "./img/paper.png";
+    cpuWeapons.appendChild(cpuButton);
+    cpuButton.appendChild(img);
   }
+  else if (computerSelection === "scissors") {
+    img.src = "./img/scissors.png";
+    cpuWeapons.appendChild(cpuButton);
+    cpuButton.appendChild(img);
+  }
+  let gameText = "";
+
+  // Determine Winner
+  if ((humanSelection === 'rock' && computerSelection === 'scissors') ||
+    (humanSelection === 'paper' && computerSelection === 'rock') ||
+    (humanSelection === 'scissors' && computerSelection === 'paper')) {
+    humanScore++;
+    // Set max games to 5 --> will need some type of reset
+    if (humanScore === 5) {
+      gameText = `Congrats you win ${humanScore} - ${cpuScore}`
+      disableBtn();
+    } else {
+      gameText = `You win ${humanSelection} beats ${computerSelection}`;
+    }
+  }
+  else if (humanSelection === computerSelection) {
+    gameText = `Tie game`;
+  }
+  else {
+    cpuScore++;
+    if (cpuScore === 5) {
+      gameText = `Sorry you lose ${cpuScore} - ${humanScore}`
+      disableBtn();
+    } else {
+      gameText = `You lose ${computerSelection} beats ${humanSelection}`;
+    }
+  }
+
+  // Add score and text into HTML document
+  document.getElementById('human-score').innerHTML = humanScore;
+  document.getElementById('cpu-score').innerHTML = cpuScore;
+  document.getElementById('text-content').innerHTML = gameText;
 }
+// Select Human Choice with Click
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    humanSelection = button.id;
+    playRound();
+  })
+})
 
-game();
-winnerMessage();
-console.log(humanWins);
-console.log(computerWins);
+// Rotate on click 
+rotateImgs.forEach((rotateImg) => {
+  rotateImg.addEventListener('click', () => {
+    rotateImg.classList.add('spin');
+    setTimeout(() => {
+      rotateImg.classList.remove("spin")
+    }, 500)
+  })
+})
+
+// Restart Game
+restart.addEventListener('click', () => {
+  document.getElementById('human-score').innerHTML = 0;
+  document.getElementById('cpu-score').innerHTML = 0;
+  document.getElementById('text-content').innerHTML = "Rock Beats Scissors | Scissors Beats Paper | Paper Beats Rock"
+  rotateImgs.forEach(rotateImg => rotateImg.classList.remove('noanimation'));
+  img.remove();
+  cpuButton.remove();
+  humanScore = 0;
+  cpuScore = 0;
+  buttons.forEach(button => button.disabled = false);
+})
+
+
